db780868da6fc6e61402d4bd0fd06e1ffe3893e0
Mando <hey@armandohimself.com>
2025-04-30 16:50:36 -0500

    chore: test main app with simple search component and added typescript safety for imports
    - linter was yelling at me for case sensitivity mismatch in import path. Fixed with config to force consistent casing in file names set to true

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Search from "./components/Search";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">App</h1>;
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+
+          <img src="./hero.png" alt="Hero Banner" />
+        </header>
+
+        <Search></Search>
+      </div>
+    </main>
+  );
 };
 
 export default App;
