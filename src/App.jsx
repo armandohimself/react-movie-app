a895dc28c4de1bf904a4769d1627d5c78fac6778
Mando <hey@armandohimself.com>
2025-04-29 22:08:48 -0500

    Init commit-rebase continue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 
+// WIP
+// const RandomStyle = () => {
+//   useEffect(() => {
+//     const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//     document.documentElement.style.setProperty(
+//       "--random-color",
+//       `#${randomColor}`
+//     );
+//   }, []);
+// };
+
 const Card = ({ title }) => {
   return (
     <>
-      <div>Card Component</div>
-      <div>{title}</div>
+      <div className="card-title">
+        <h2>{title}</h2>
+      </div>
+      <div className="card-body">Lorem ipsum...</div>
     </>
   );
 };
@@ -13,8 +26,12 @@ const App = () => {
   return (
     <div>
       <h2>Functional Arrow Component</h2>
-      <Card title="Batman" />
-      <Card title="Superman VS Batman" />
+      <div className="card-container">
+        <div className="card-content">
+          <Card title="Batman" />
+          <Card title="Superman VS Batman" />
+        </div>
+      </div>
     </div>
   );
 };
