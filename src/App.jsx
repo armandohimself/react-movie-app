ffae3106e2e8c70f430443c3ee9ecea6f469db8e
Mando <hey@armandohimself.com>
2025-05-01 15:39:59 -0500

    feat: implement movie fetching logic and enhance error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/Search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+  const API_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+
+  const fetchMovies = async () => {
+    setisLoading(true);
+    setErrorMessage("");
+
+    try {
+      // Shape the fetch
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+      };
+
+      // Attempt to fetch response object
+      const response = await fetch(endpoint, options);
+
+      // Guard clause response not being okay
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      // Attempt to parse response to json so we can use it
+      const data = await response.json();
+
+      // Guard clause the parsing going wrong
+      if (data.Response === "False") {
+        setMovieList([]);
+        setErrorMessage(data.Error || "Failed to parse movies response object");
+        return;
+      }
+
+      setMovieList(data.results || []);
+
+      setisLoading(false);
+    } catch (error) {
+      console.log(`Error fetching movies ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setisLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
@@ -14,9 +71,28 @@ const App = () => {
           </h1>
 
           <img src="./hero.png" alt="Hero Banner" />
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search></Search>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
+
+        <h1>{searchTerm}</h1>
       </div>
     </main>
   );
